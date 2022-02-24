3b6f7a67322c8736168889a02cfc02fed5c62687
Samuel Cardoso <89113186+samuell2@users.noreply.github.com>
2022-02-24 15:17:20 -0300

    Update OrdenacaodeVetor.cs

--- OrdenacaodeVetor.cs
+++ OrdenacaodeVetor.cs
@@ -2,43 +2,49 @@ using System;
 
 namespace OrdenacaoDeVetor
 {
-    int[] numeros = new int[5] { 29, 10, 14, 37, 14 };
-
-    int tmp, j;
-
-    //ordem crescente
-    for (int i = 1; i < numeros.Length; i++)
+    public class Program
     {
-        tmp = numeros[i];
-        for (j = i-1; j >= 0 && tmp < numeros[j]; j--)
+        static void Main(string[] args)
         {
-            numeros[j+1] = numeros[j];
+            int[] numeros = new int[5] { 29, 10, 14, 37, 14 };
+
+            int tmp, j;
+
+            //ordem crescente
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                tmp = numeros[i];
+                for (j = i-1; j >= 0 && tmp < numeros[j]; j--)
+                {
+                 numeros[j+1] = numeros[j];
+              }
+              numeros[j + 1] = tmp;
+         }
+
+          Console.WriteLine("Em ordem crescente: ");
+          for (int i = 0; i < numeros.Length; i++)
+         {
+                Console.Write($"{numeros[i]}, ");
+         }
+
+         Console.WriteLine();
+
+          //ordem decrescente
+            for (int i = 1; i < numeros.Length; i++)
+         {
+              tmp = numeros[i];
+              for (j = i - 1; j >= 0 && tmp > numeros[j]; j--)
+             {
+                    numeros[j + 1] = numeros[j];
+             }
+             numeros[j + 1] = tmp;
+            }
+
+         Console.WriteLine("Em ordem decrescente: ");
+         for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write($"{numeros[i]}, ");
+            }  
         }
-        numeros[j + 1] = tmp;
     }
-
-    Console.WriteLine("Em ordem crescente: ");
-    for (int i = 0; i < numeros.Length; i++)
-    {
-        Console.Write($"{numeros[i]}, ");
-    }
-
-    Console.WriteLine();
-
-    //ordem decrescente
-    for (int i = 1; i < numeros.Length; i++)
-    {
-        tmp = numeros[i];
-        for (j = i - 1; j >= 0 && tmp > numeros[j]; j--)
-        {
-            numeros[j + 1] = numeros[j];
-        }
-        numeros[j + 1] = tmp;
-    }
-
-    Console.WriteLine("Em ordem decrescente: ");
-    for (int i = 0; i < numeros.Length; i++)
-    {
-        Console.Write($"{numeros[i]}, ");
-    }  
 }
